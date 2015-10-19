87450280571cc45ef1c045ded16d56f16acb7fef
Chris Nesbitt-Smith <chris@cns.me.uk>
2015-10-19 12:28:32 +0100

    appears to be working to current spec

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # sails-controller-driven-routing
 A routing configuration similar to blueprint that reads the routes from the controller so everything is kept in one place
+
+It relies on blueprints validating your configuration (because I didn't want to copy/paste), but you could maybe disable blueprints entirely in production if you're feeling courageous and don't need any of its helpers.
+
+## Motivation
+* I wanted to support have a controller that has `n` accessible points
+* I wanted to export my private methods to allow for easy testing (I just discovered [Rewire](https://www.npmjs.com/package/rewire) so that's probably no longer true).
+* I didn't want those private methods to be exposed to http
+* I wanted to keep all the configuration in the controller rather than updating the `routing.js`
+* I wanted to handle multiple HTTP verbs without having to have a `switch()` at the top of each exposed method.
+
+## Usage
+
+`$ npm instal sails-controller-driven-routing`
+Your controller can now look like the following:
+
+MyController.js
+```javascript
+module.exports = {
+  _config: {
+    actions: false, //disable built in blueprints
+    shortcuts: false, //disable built in blueprints
+    rest: false, //disable built in blueprints
+    explicitRoutes: {
+      'OPTIONS /hello/world': 'mycontroller.world'
+    },
+    exposedMethods: [
+      'foobar'
+    ]
+  },
+  world: function(res, req) {
+    return res.ok("hello");
+  },
+  foobar: function(res, req) {
+    return res.ok("bar");
+  },
+  foobarPost: function(res, req) {
+    return res.ok("par");
+  },
+};
+```
+
+Now:
+`OPTIONS /hello/world` will respond with `hello`
+
+`GET/POST/DELETE/OPTIONS/PUT/etc /mycontroller/foobar` will respond with `bar`
+
+`POST /mycontroller/foobar` will respond with `par`
+
+You can also specify a `prefix` that'll work the same way as blueprints ones do which will override the 'mycontroller' segment of the url for the `exposedMethods` section.
+
+# TODO:
+- [ ] unit tests
+
+Hopefully this will prove useful to someone else, pull requests welcome if you want to tidy it up a bit :)
---
+++ index.js
@@ -0,0 +1,103 @@
+var _ = require('lodash');
+
+var http_verbs = ['get', 'options', 'head', 'post', 'put', 'delete'];
+
+module.exports = function (sails) {
+
+  /**
+   * Expose blueprint hook definition
+   */
+  return {
+    defaults: {
+      controllerDrivenRoutes: {
+        prefix: '',
+        routes: {},
+      }
+    },
+
+    initialize: function (cb) {
+
+      var eventsToWaitFor = [];
+      eventsToWaitFor.push('router:after');
+      if (sails.hooks.policies) {
+        eventsToWaitFor.push('hook:policies:bound');
+      }
+      if (sails.hooks.controllers) {
+        eventsToWaitFor.push('hook:controllers:loaded');
+      }
+      sails.after(eventsToWaitFor, this.bindShadowRoutes);
+      return cb();
+    },
+
+    bindShadowRoutes: function () {
+
+      _.each(sails.middleware.controllers, function eachController(controller, controllerId) {
+        if (!_.isObject(controller) || _.isArray(controller)) return;
+
+        // Get globalId for use in errors/warnings
+        var globalId = sails.controllers[controllerId].globalId;
+
+        // Determine blueprint configuration for this controller
+        var config = _.merge(
+          sails.config.controllerDrivenRoutes,
+          controller._config || {});
+
+        // Validate blueprint config for this controller
+        if (config.prefix && !config.prefix.match(/^\//)) {
+          var originalPrefix = config.prefix;
+          config.prefix = '/' + config.prefix;
+        }
+
+        // Determine base route
+        var baseRoute = config.prefix + '/' + controllerId;
+
+        if (config.pluralize) {
+          baseRoute = pluralize(baseRoute);
+        }
+
+        // map any specfic routes that are in the controller
+        _.map(config.explicitRoutes, function (action, route) {
+          sails.log.silly('Binding action (' + action.toLowerCase() + ') blueprint/shadow route for controller:', controllerId);
+          var opts = {
+            controller: controllerId
+          }
+          if (_.isString(action)) {
+            opts.action = action;
+          }
+          else {
+            opts = _.merge(opts, action);
+          }
+          sails.router.bind(route, action);
+        });
+
+        // map the exposed methods of the controller
+        _.each(config.exposedMethods, function (method) {
+          var endpoint = method != 'index' ? method : '';
+
+          if (controller[method] !== undefined) {
+            sails.log.silly('Binding ' + baseRoute + "/" + endpoint + ' to ' + controllerId + '.' + method);
+            sails.router.bind(baseRoute + "/" + endpoint, {
+              controller: controllerId,
+              action: method
+            });
+          }
+          ;
+          //handle the verbs
+          _.each(http_verbs, function (verb) {
+            var candidateMethod = method + verb;
+            // handle special case for index
+            if (controller[candidateMethod] !== undefined) {
+              sails.log.silly('Binding ' + verb.toUpperCase() + " " + baseRoute + "/" + endpoint + ' to ' + controllerId + '.' + candidateMethod);
+              sails.router.bind(verb.toUpperCase() + " " + baseRoute + "/" + endpoint, {
+                controller: controllerId,
+                action: candidateMethod
+              });
+            }
+            ;
+          });
+        });
+      });
+    }
+  };
+};
+
